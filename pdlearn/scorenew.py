cbfd015a5719241a29c601b0254a416f359e9010
Kevin-lou <loukuofeng@163.com>
2019-09-20 10:17:57 +0800

    1、注释掉等待10秒，加快速度 2、修正了有效浏览和有效视听不精确的问题，把完成了几条计算进去

--- pdlearn/scorenew.py
+++ pdlearn/scorenew.py
@@ -4,7 +4,7 @@ def get_score(driver):
     try:
         driver.get_url('https://pc.xuexi.cn/points/my-points.html')
         driver.web_wait(270, u"我的积分")
-        time.sleep(10)
+        # time.sleep(10)
         driver1 = driver.in_driver()
         total = driver1.find_element_by_css_selector('div.my-points-block')
         total = total.text.splitlines()
@@ -27,17 +27,22 @@ def get_diandian(driver):
         driver.get_url('https://pc.xuexi.cn/points/ptp.html')
         driver.web_wait(270, u"我的点点通")
         # driver.find_element_by_css_selector('button.ant-btn.ant-btn-primary').click()
-        time.sleep(10)
+        # time.sleep(10)
         driver2 = driver.in_driver()
         total = driver2.find_element_by_css_selector('div.my-points-block')
         total = total.text.splitlines()
         mydian = {'点点通': total[1]}
-        each = driver2.find_elements_by_css_selector('div.my-points-card-text')
+        each1 = driver2.find_elements_by_css_selector('div.my-points-card-text')
+        each2 = driver2.find_elements_by_css_selector('div.my-points-card-subtitle')
         name = ['有效浏览', '有效视听']
         for i in range(len(name)):
-            mydian.update({name[i]: int(each[i].text[0]) * 6})
+            a = int(each1[i].text[0])
+            if a == 0:
+                mydian.update({name[i]:a *6 + int(each2[i].text[19])})
+            else:
+                mydian.update({name[i]:(a-1) *6 + int(each2[i].text[19])})
         for i in range(len(name)):
-            mydian.update({name[i]+'目标': int(each[i].text[3]) * 6})
+            mydian.update({name[i]+'目标': int(each1[i].text[3]) * 6})
         return mydian
     except:
         print("=" * 120)
